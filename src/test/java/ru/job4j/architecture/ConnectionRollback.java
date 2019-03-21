b6d6ba77aee8d6cf79a82d31ec4c2b35e73eb526
AlexandrKaleganov <alexmur07@mail.ru>
2019-03-21 15:11:30 +0700

    Переделать тесты на роллбек

---
+++ src/test/java/ru/job4j/architecture/ConnectionRollback.java
@@ -0,0 +1,27 @@
+package ru.job4j.architecture;
+
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionRollback {
+
+        public static Connection create(Connection connection) throws SQLException {
+            connection.setAutoCommit(false);
+            return (Connection) Proxy.newProxyInstance(
+                    ConnectionRollback.class.getClassLoader(),
+                    new Class[]{Connection.class},
+                    (proxy, method, args) -> {
+                        Object rsl = null;
+                        if ("close".equals(method.getName())) {
+                            connection.rollback();
+                            connection.close();
+                        } else {
+                            rsl = method.invoke(connection, args);
+                        }
+                        return rsl;
+                    }
+            );
+        }
+    }
+
