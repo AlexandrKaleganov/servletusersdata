4343989b05fba834eee11a1c7e7403cb859c06da
AlexandrKaleganov <alexmur07@mail.ru>
2019-04-03 14:49:56 +0700

    Обновление скриптов, пора приступать к написанию кода

--- db/scripts/update_006.sql
+++ db/scripts/update_006.sql
@@ -1,4 +1,4 @@
---основная база услугЖ наименование, количество и цена за еденицу
+--основная база услуг наименование, количество и цена за еденицу
 --insert into service(name, maxcount, price) values ('картридж', 90, 375.50);
 create table if not exists  service(
 id serial primary key,
@@ -9,12 +9,13 @@ date timestamp not null default now()
 );
 --insert into service(name, maxcount, price) values ('картридж', 90, 375.50);
 --insert into service(name, maxcount, price) values ('картридж2', 90, 375.50);
---лист, который я буду получать, лист  всех услуг
+--лист, который я буду получать, лист  всех услуг с ценой
 create or replace view  summary as (
 select id, name, maxcount, price, (maxcount * price) as summ , date from service
 );
 -- тут будут ханится счета, которые нам будут отправлять на утверждение после заправки картриджей(после оказанных услу)
 --если всё верно и нас устраивает  мы будем жать утвердить и данные будут применены в другую таблицу а эта таблица будет очищаться
+--команда очистки: delete from service_temp where numberschet = 123;  удалять буду по счёту в случае применения
 --insert into service_temp (numberschet, id_service, maxcount) values(123, 1, 8);
 create table if not exists  service_temp(
 numberschet integer,
