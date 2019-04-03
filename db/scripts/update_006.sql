ca531eb449c4d734418dcccb73918714c85d5c19
AlexandrKaleganov <alexmur07@mail.ru>
2019-04-03 11:57:05 +0700

    тест обновление скриптов

--- db/scripts/update_006.sql
+++ db/scripts/update_006.sql
@@ -7,6 +7,8 @@ maxcount integer,
 price money,
 date timestamp not null default now()
 );
+insert into service(name, maxcount, price) values ('картридж', 90, 375.50);
+insert into service(name, maxcount, price) values ('картридж2', 90, 375.50);
 --лист, который я буду получать, лист  всех услуг
 create or replace view  summary as (
 select id, name, maxcount, price, (maxcount * price) as summ , date from service
@@ -20,6 +22,7 @@ id_service integer references  service(id),
 maxcount integer,
 date timestamp not null default now()
 );
+insert into service_temp (numberschet, id_service, maxcount) values(123, 1, 8);
 -- сюда будем добавлять одобренные услуги(утверждённые счета)
 --insert into service_res (numberschet, id_service, maxcount, date) values(123, 1, 8, '03-04-2019');
 create table if not exists  service_res(
@@ -28,3 +31,7 @@ id_service integer references  service(id),
 maxcount integer,
 date timestamp
 );
+insert into service_res (numberschet, id_service, maxcount, date) values(123, 1, 8, '03-04-2019');
+--моя вью таблица отслеживающая данные
+select s.id, s.name, (s.maxcount - res.maxcount) as ost_count, s.price, ((s.maxcount - res.maxcount) * s.price) as ost_money from service as s
+left outer join service_res as res on s.id = res.id_service;
